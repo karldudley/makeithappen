3ff980746d034465ce883f8a263f8aa7bdc57fba
karl dudley <karldudley@gmail.com>
2022-09-16 21:50:39 +0100

    change server message1

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "dev") {
 
 // redirect or display message?
 server.get('/', (req, res) => {
-    res.send("<h1 style=\"color:#D81159;\">Welcome to the Make it Happen server</h1><h2>The following endpoints currently exist:</h2><h3>GET /habits</h3><h3>GET /habits/:id</h3><h3>POST /habits/</h3><h3>DELETE /habits/:id</h3><h3>PATCH /habits/:id</h3><h3>POST /users/signup</h3><h3>PATCH /habits/:id</h3><h3>POST /users/login</h3>")
+    res.send("<h1 style=\"color:#D81159;\">Welcome to the Make it Happen server</h1><h2>The following endpoints currently exist:</h2><h3>GET /habits</h3><h3>GET /habits/:id</h3><h3>POST /habits/</h3><h3>DELETE /habits/:id</h3><h3>PATCH /habits/:id</h3><h3>POST /users/signup</h3><h3>POST /users/login</h3>")
     // res.redirect('/habits');
   });
 
