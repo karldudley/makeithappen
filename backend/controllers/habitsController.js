6c0b0b1672e7aa3a745d6f5220bcf2e10f42c655
Duncan <duncan.cornish.1999@gmail.com>
2022-09-16 11:57:43 +0100

    add http status codes

--- backend/controllers/habitsController.js
+++ backend/controllers/habitsController.js
@@ -1,22 +1,22 @@
 //get all habits
 const getHabits = async (req, res) => {
     //connect to Mongo using mongoose
-    res.send("Got all habits")
+    res.status(200).send("Got all habits")
 }
 
 const getHabitById = async (req, res) => {
     //connect to Mongo using mongoose
-    res.send(`Got habit ${req.params.id}`)
+    res.status(200).send(`Got habit ${req.params.id}`)
 }
 
 const createHabit = async (req, res) => {
     //connect to Mongo using mongoose
-    res.send('Created new habit')
+    res.status(201).send('Created new habit')
 }
 
 const destroyHabit = async (req, res) => {
     //connect to Mongo using mongoose
-    res.send('Destoryed habit')
+    res.status(204).send('Destoryed habit')
 }
 
 module.exports = {getHabits, getHabitById, createHabit, destroyHabit};
