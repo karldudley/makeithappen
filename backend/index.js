85ef31e77428eb5af347d34df363fb04a8d001c1
karl dudley <karldudley@gmail.com>
2022-09-16 16:01:53 +0100

    debug heroku deployment1

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
 const app = require('./server');
 const mongoose = require('mongoose')
-// require('dotenv').config()
+require('dotenv').config()
 const port = process.env.PORT || 3000;
-const MONGO_URI= "mongodb+srv://futureproof:fppass@makeithappen.azatkv3.mongodb.net/?retryWrites=true&w=majority"
 
 //connection to mongo and start to listen
-mongoose.connect(MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(port, () => {
             console.log(`Connected to DB and Express now departing from port ${port}!`)
