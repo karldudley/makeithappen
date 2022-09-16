000027eb28cd119800222caadf7b4971f5fa6038
karl dudley <karldudley@gmail.com>
2022-09-16 17:47:16 +0100

    redirect users to /habits if they go to root domain

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,19 @@ server.use(cors());
 server.use(express.json())
 
 // morgan
-// const morgan = require('morgan')
-// if (process.env.NODE_ENV === 'development') {
-//   app.use(morgan('dev'))
-// }
+const morgan = require('morgan')
+if (process.env.NODE_ENV === 'development') {
+  server.use(morgan('dev'))
+}
+
+// redirect users to habits if they go to root domain
+server.get('/', (req, res) => {
+    // res.send("Hello!")
+    res.redirect('/habits');
+  });
 
 // routes
 server.use('/habits', habitRoutes)
+
 
 module.exports = server;
