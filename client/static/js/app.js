598d85f644d574f9e308b163f5e196f0fb9cbecd
karl dudley <karldudley@gmail.com>
2022-09-21 15:10:43 +0100

    latest version

--- client/static/js/app.js
+++ client/static/js/app.js
@@ -261,7 +261,7 @@ async function sendDelete(id) {
                     'Authorization': `Bearer ${token}`,
                     "Content-Type": "application/json"
                 }
-
+    
             }
             const response = await fetch(`https://make-it-happen-fp.herokuapp.com/habits/${id}`, options);
             const data = await response.json();
